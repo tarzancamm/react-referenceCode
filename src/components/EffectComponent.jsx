e2b2df8a3aae639e9d5cb57b9ef3ea4f7fd5e443
Cameron Miner <cameronsminer@gmail.com>
2022-12-02 15:59:37 -0700

    Created references for useState and useEffect

---
+++ src/components/EffectComponent.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const EffectComponent = () => {
+    const [data, setData] = useState({})
+
+    const getData = () => {
+        axios.get('https://www.swapi.tech/api/people/1').then((res) => {
+            setData(res.data.result.properties)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return (
+        <div className='card'>
+            <h2>useEffect</h2>
+            <h3>Name: {data.name}</h3>
+        </div>
+    )
+}
+
+export default EffectComponent
